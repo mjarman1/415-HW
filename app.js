041b001ac131788ac36688bacbb4935c651044cc
Marquis <Marquis.jayshaun.jarman@gmail.com>
2021-04-15 08:54:17 -0400

    hw push

---
+++ app.js
@@ -0,0 +1,23 @@
+var a = 1
+var b = 5
+var c = 10
+var d = 15
+var e = 20
+var f = 25
+var g = 30
+var h = 35
+var i = 40
+var j = 45
+
+
+console.log(a+J) 
+console.log(f+I)
+
+console.log(h-F)
+console.log(j-c)
+
+console.log(i/j)
+console.log(e/c)
+
+console.log(d*b)
+console.log(j*f)
